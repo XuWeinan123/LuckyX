c45cbe1c55caa1e931af34e0c0882652295346ad
xwn <woshixwn@gmail.com>
2018-11-18 08:56:42 +0800

    Initial Commit

---
+++ LuckyX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LuckyX
+//
+//  Created by 徐炜楠 on 2018/11/18.
+//  Copyright © 2018 徐炜楠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
